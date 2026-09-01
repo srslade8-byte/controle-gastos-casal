977744c3205bd90e3f002deec17dcb780ac08de6
srslade8-byte <srslade8@gmail.com>
2026-09-01 06:50:39 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NOME = 'controle-de-gastos-v5';
+const ARQUIVOS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (evento) => {
+  evento.waitUntil(
+    caches.open(CACHE_NOME).then((cache) => cache.addAll(ARQUIVOS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evento) => {
+  evento.waitUntil(
+    caches.keys().then((nomes) =>
+      Promise.all(nomes.filter((n) => n !== CACHE_NOME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evento) => {
+  evento.respondWith(
+    caches.match(evento.request).then((resposta) => {
+      return resposta || fetch(evento.request).then((res) => {
+        if (evento.request.method === 'GET' && res.ok) {
+          const copia = res.clone();
+          caches.open(CACHE_NOME).then((cache) => cache.put(evento.request, copia));
+        }
+        return res;
+      }).catch(() => resposta);
+    })
+  );
+});
